cfbcda663108ae7cbe0031781e478f4babdac3fd
coderZel0 <obitouchiha0026@gmail.com>
2025-03-27 22:53:38 +0530

    SDL setup

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
+#include "SDL3/SDL.h"
+#include "SDL3/SDL_log.h"
 #include "renderer.h"
 #include <iostream>
 
 using namespace std;
 
 int main() {
-  cout << "Hello World!" << endl;
+  SDL_Log("%s", "HELLO SDL");
   NRENDERER::Renderer renderer;
   renderer.setWidth(100);
   cout << renderer.getWidth() << endl;
